7c1454cbfed29bca2d5d5bd0d25213b747ccad1a
Peter Söderberg <81304517+petsod8@users.noreply.github.com>
2021-03-29 12:47:10 +0200

    Add files via upload

---
+++ Uppgift 4/Date/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Date
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			DateTime now = DateTime.Today;
+			Console.WriteLine("Today's date is {0} \n", now);
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Uppgift 5/BiggestNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BiggestNumber
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{	
+			Console.Write("Skriv ett nummer: ");
+			int tal1 = Convert.ToInt32(Console.ReadLine());
+			//{ 8, 1, 3, 5 };
+			Console.Write("Skriv ett nummer till: ");
+			int tal2 = Convert.ToInt32(Console.ReadLine());
+
+			if (tal1 < tal2) 
+			{ 
+				Console.WriteLine("Det största talet är : " + tal2); 
+			} 
+			else if(tal2 < tal1) 
+			{ 
+				Console.WriteLine("Det största talet är : " + tal1); 
+			} 
+			else 
+			{ 
+				Console.WriteLine("De är samma tal : " + tal2+ " " +tal1); 
+			}
+
+			Console.ReadKey();
+
+		}
+	}
+}
