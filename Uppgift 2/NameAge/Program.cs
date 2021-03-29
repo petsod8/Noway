4674c8cc1b20a09b1f506d6c0dafe1acbafa5295
Peter Söderberg <81304517+petsod8@users.noreply.github.com>
2021-03-29 12:44:15 +0200

    Add files via upload

---
+++ Uppgift 2/NameAge/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NameAge
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("First name: ");
+			String firstname = Console.ReadLine();
+			Console.Write("Surname: ");
+			String lastname = Console.ReadLine();
+			Console.Write("Age: ");
+			String age = Console.ReadLine();
+			Console.WriteLine("Hello " + firstname +" "+ lastname + " your age is " + age);
+			Console.WriteLine("Enter a key to exit");
+			Console.ReadKey();
+		}
+	}
+}
