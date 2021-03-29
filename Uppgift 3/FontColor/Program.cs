5e41ecdebac2c171d013143c3875e4593eaa8783
Peter Söderberg <81304517+petsod8@users.noreply.github.com>
2021-03-29 12:45:45 +0200

    Add files via upload

---
+++ Uppgift 3/FontColor/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FontColor
+{
+	class Program
+	{
+		static void Main()
+		{
+			Console.ForegroundColor = ConsoleColor.DarkRed;
+			Console.WriteLine("Hello World!");
+			Console.ReadKey();
+			Console.ForegroundColor = ConsoleColor.Gray;
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
