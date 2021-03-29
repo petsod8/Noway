169cbe8cdbceaa07268ff926cdc2ef66b316b66b
Peter Söderberg <81304517+petsod8@users.noreply.github.com>
2021-03-29 12:49:51 +0200

    Add files via upload

---
+++ Uppgift 10/MultiplicationMatrix/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MultiplicationMatrix
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int i = 1;
+			int j = 1;
+			while (i < 11)
+			{
+				while (j < 11)
+				{
+					Console.WriteLine("" + i + " * " + j + " = " + i
+
+* j);
+					j++;
+				}
+				Console.WriteLine();
+				i++;
+				j = 1;
+			}
+		}
+	}
+}
---
+++ Uppgift 9/Maths/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett decimaltal: ");
+            double value = double.Parse(Console.ReadLine());
+
+            //Roten ur
+            Console.WriteLine("Roten ur blir: " + Math.Sqrt(value) + ".");
+
+            //upphöjt till 2
+            Console.Write("Upphöjt till 2 blir svaret: ");
+            Console.WriteLine($"{value}^2 = {(long)Math.Pow(value, 2):N0}");
+
+
+            //upphöjt till 10
+            Console.Write("Upphöjt till 10 blir svaret: "); 
+            Console.WriteLine($"{value}^10 = {(long)Math.Pow(value, 10):N0}");
+
+            Console.ReadKey();
+        }
+    }
+}
